5d61cda5d352fc39c43745f75424f3fdb2dd0ab2
Harrison Preston <harrisonpreston@harrisons-mbp.lan>
2020-07-25 16:29:58 -0700

    Created Bar and Pie charts using Plotly

---
+++ data-12-1-2-resources/plots.js
@@ -0,0 +1,45 @@
+var trace = {
+    x: ["burrito", "pizza", "chicken"],
+    y: [10, 18, 5],
+    type: "bar"
+};
+
+var layout = {
+    title: "Luncheon Survey",
+    xaxis: {title: "Food Option"},
+    yaxis: {title: "Number of Respondents"}
+};
+
+Plotly.newPlot("plotArea", [trace], layout);
+
+// Skill Drill
+
+var trace = {
+    x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"], 
+    y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+    type: "bar"
+};
+
+var layout = {
+    title: "Beverage Chart",
+    xaxis: {title: "Drinks"}, 
+    yaxis: {title: "% Drinks Ordered"}
+};
+
+Plotly.newPlot("plotArea", [trace], layout);
+
+// Create a Pie chart
+
+var trace = {
+    labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
+    "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+    values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+    type: 'pie'
+   };
+   var data = [trace];
+   var layout = {
+    title: "'Bar' Chart",
+   };
+   Plotly.newPlot("plotArea", data, layout);
+
+ 
