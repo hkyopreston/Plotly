6f33345bca7af5bd6b1d33597f4751dee100492e
Harrison Preston <harrisonpreston@Harrisons-MBP.T-mobile.com>
2020-07-27 16:38:17 -0700

    Fastest Growing Cities Data

---
+++ data-12-2-2-resources/static/js/plot.js
@@ -0,0 +1,17 @@
+console.log(cityGrowths);
+
+var topFiveCityNames = topFiveCities.map(city => city.City);
+var topFiveCityGrowths = topFiveCities.map(city => parseInt(city.Increase_from_2016));
+
+var trace = {
+    x: topFiveCityNames,
+    y: topFiveCityGrowths,
+    type: "bar"
+  };
+  var data = [trace];
+  var layout = {
+    title: "Most Rapidly Growing Cities",
+    xaxis: { title: "City" },
+    yaxis: { title: "Population Growth, 2016-2017"}
+  };
+  Plotly.newPlot("bar-plot", data, layout);
